36503a2b64490e2bc313d8c09aed78d39db23311
PancakeTAS <pancake@mgnet.work>
2025-10-04 16:47:25 +0200

    pkgs(xz)

---
+++ pkgs/xz.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# metadata
+pkgname="xz"
+pkgver="5.8.1"
+pkgdesc="command line tools for XZ and LZMA compressed files"
+pkgurl="https://tukaani.org/xz/"
+pkglic="BSD"
+
+# build information
+pkgdeps=(
+    "compiler-rt-21.1.2"
+    "linux-6.16.9"
+    "musl-1.2.5"
+)
+pkgsrcs=(
+    "https://github.com/tukaani-project/$pkgname/releases/download/v$pkgver/$pkgname-$pkgver.tar.xz"
+)
+
+# build scripts
+pkgprepare() {
+    cd $pkgname-$pkgver
+
+    ./configure \
+        --prefix=/usr \
+        --host=aarch64-dog-linux-musl \
+        --disable-doc \
+        --disable-static \
+        --disable-nls \
+        --disable-rpath \
+        --enable-year2038 \
+        --with-sysroot="$buildroot" \
+        CC=clang LD=ld.lld \
+        CFLAGS="-O3" LDFLAGS="-flto" \
+        AR=llvm-ar RANLIB=llvm-ranlib STRIP=llvm-strip OBJDUMP=llvm-objdump MANIFEST_TOOL=llvm-mt NM=llvm-nm
+}
+
+pkgbuild() {
+    make
+}
+
+pkginstall() {
+    make install-strip DESTDIR="$pkgroot"
+
+    rm -rf \
+        "$pkgroot/usr/share" \
+        "$pkgroot/usr/lib/pkgconfig" \
+        "$pkgroot/usr/lib/liblzma.la"
+}
