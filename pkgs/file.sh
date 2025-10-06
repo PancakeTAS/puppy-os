3698926472ef76411402387a61c669a1f6060eb3
PancakeTAS <pancake@mgnet.work>
2025-10-06 19:09:59 +0200

    pkgs(file):

--- pkgs/file.sh
+++ pkgs/file.sh
@@ -1,41 +1,39 @@
 #!/bin/sh
 
 # metadata
-pkgname="procps-ng"
-_pkgname="procps"
-pkgver="4.0.5"
-pkgdesc="utilities for system monitoring and process management"
-pkgurl="https://gitlab.com/procps-ng/procps"
-pkglic="GPLv2"
+pkgname="file"
+pkgver="5.46"
+pkgdesc="file type identification utility"
+pkgurl="https://www.darwinsys.com/file/"
+pkglic="file"
 
 # build information
 pkgdeps=(
     "linux-headers-6.16.9"
-    "libcxx-21.1.2"
     "musl-1.2.5"
-    "ncurses-6.5"
+    "zlib-1.3.1"
+    "bzip2-1.0.8"
+    "xz-5.8.1"
+    "zstd-1.5.7"
 )
 pkgsrcs=(
-    "https://gitlab.com/$pkgname/$_pkgname/-/archive/v$pkgver/$_pkgname-v$pkgver.tar.gz"
+    "https://github.com/file/file/archive/refs/tags/FILE5_46.tar.gz"
 )
 
 # build scripts
 pkgprepare() {
-    cd $_pkgname-v$pkgver
+    cd $pkgname-FILE5_46
 
-    sed -i 's/AC_FUNC_MALLOC//' configure.ac
-    sed -i 's/AC_FUNC_REALLOC//' configure.ac
-
-    ./autogen.sh
+    autoreconf -fiv
     ./configure \
         --prefix=/usr \
         --host=aarch64-dog-linux-musl \
         --disable-dependency-tracking \
-        --disable-nls \
         --disable-static \
-        --disable-rpath \
-        --enable-watch8bit \
-        --enable-year2038 \
+        --enable-zlib \
+        --enable-bzlib \
+        --enable-xzlib \
+        --enable-zstdlib \
         --with-sysroot="$buildroot" \
         CC=clang LD=ld.lld \
         CFLAGS="-O3" LDFLAGS="-flto" \
@@ -49,8 +47,8 @@ pkgbuild() {
 pkginstall() {
     make install-strip DESTDIR="$pkgroot"
 
-    rm -rv \
+    rm -rf \
         "$pkgroot/usr/share" \
         "$pkgroot/usr/lib/pkgconfig" \
-        "$pkgroot/usr/lib/libproc2.la"
+        "$pkgroot/usr/lib/libmagic.la"
 }
