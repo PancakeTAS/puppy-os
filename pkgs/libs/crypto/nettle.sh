9652a6f86997bce2d2b8a9a638158ab76d1e3908
PancakeTAS <pancake@mgnet.work>
2025-10-11 05:01:44 +0200

    pkgs: nettle

---
+++ pkgs/libs/crypto/nettle.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+pkgname="nettle"
+pkgver="3.10.2"
+pkgsrcs=(
+    "https://ftpmirror.gnu.org/gnu/$pkgname/$pkgname-$pkgver.tar.gz"
+) # NOT A GNU PROJECT!! THEY JUST HAVE HTTPS RELEASES!
+
+pkgprepare() {
+    cd $pkgname-$pkgver
+
+    ./configure \
+        --prefix=/usr \
+        --sbindir=/usr/bin \
+        --libexecdir=/usr/lib \
+        --sysconfdir=/etc \
+        --host=aarch64-dog-linux-musl \
+        --disable-dependency-tracking \
+        --disable-static \
+        --enable-mini-gmp \
+        --with-sysroot="$sysroot" \
+        CC=clang CXX=clang++ LD=ld.lld \
+        CFLAGS="-O3" LDFLAGS="-flto" \
+        AR=llvm-ar RANLIB=llvm-ranlib STRIP=llvm-strip OBJDUMP=llvm-objdump DLLTOOL=llvm-dlltool MANIFEST_TOOL=llvm-mt NM=llvm-nm
+}
+
+pkgbuild() {
+    make
+}
+
+pkginstall() {
+    make DESTDIR="$pkgdir" install
+}
