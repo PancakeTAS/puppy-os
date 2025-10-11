29fd59e9ffe23610a7fbb75dc024a9f5a8686c91
PancakeTAS <pancake@mgnet.work>
2025-10-11 03:56:42 +0200

    pkgs: less

---
+++ pkgs/utils/less.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+pkgname="less"
+pkgver="679"
+pkgsrcs=(
+    "https://www.greenwoodsoftware.com/$pkgname/$pkgname-$pkgver.tar.gz"
+)
+
+pkgprepare() {
+    cd $pkgname-$pkgver
+
+    ./configure \
+        --prefix=/usr \
+        --sbindir=/usr/bin \
+        --libexecdir=/usr/lib \
+        --host=aarch64-dog-linux-musl \
+        --disable-dependency-tracking \
+        --disable-nls \
+        --disable-rpath \
+        --with-sysroot="$sysroot" \
+        CC=clang LD=ld.lld \
+        CFLAGS="-O3" LDFLAGS="-flto" \
+        AR=llvm-ar RANLIB=llvm-ranlib STRIP=llvm-strip OBJDUMP=llvm-objdump DLLTOOL=llvm-dlltool MANIFEST_TOOL=llvm-mt NM=llvm-nm
+}
+
+pkgbuild() {
+    make
+}
+
+pkginstall() {
+    make DESTDIR="$pkgdir" install-strip
+}
