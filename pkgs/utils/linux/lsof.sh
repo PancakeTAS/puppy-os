f7d0c785c2887392d25f2306f9f204e722a57a2a
PancakeTAS <pancake@mgnet.work>
2025-10-11 05:48:52 +0200

    pkgs: lsof

---
+++ pkgs/utils/linux/lsof.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+pkgname="lsof"
+pkgver="4.99.5"
+pkgsrcs=(
+    "https://github.com/lsof-org/$pkgname/releases/download/$pkgver/$pkgname-$pkgver.tar.gz"
+)
+
+pkgprepare() {
+    cd $pkgname-$pkgver
+
+    ./configure \
+        --prefix=/usr \
+        --sbindir=/usr/bin \
+        --libexecdir=/usr/lib \
+        --host=aarch64-dog-linux-musl \
+        --disable-dependency-tracking \
+        --disable-static \
+        --enable-year2038 \
+        --without-libtirpc \
+        --without-selinux \
+        --with-sysroot="$sysroot" \
+        CC=clang CXX=clang++ LD=ld.lld \
+        CFLAGS="-O3" LDFLAGS="-flto" \
+        AR=llvm-ar RANLIB=llvm-ranlib STRIP=llvm-strip OBJDUMP=llvm-objdump DLLTOOL=llvm-dlltool MANIFEST_TOOL=llvm-mt NM=llvm-nm
+}
+
+pkgbuild() {
+    make
+}
+
+pkginstall() {
+    make DESTDIR="$pkgdir" install-strip
+}
