14c32778bcfcb9d2f6c04747505069c0d1c90249
PancakeTAS <pancake@mgnet.work>
2025-10-13 02:26:13 +0200

    add btop monitoring

---
+++ pkgs/rootfs/utils/monitoring/btop.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+pkgname="btop"
+pkgver="1.4.5"
+pkgsrcs=(
+    "https://github.com/aristocratos/$pkgname/archive/refs/tags/v$pkgver.tar.gz"
+)
+
+pkgprepare() {
+    cd $pkgname-$pkgver
+
+    cmake -S . -B build -G Ninja \
+        -DCMAKE_INSTALL_PREFIX="$pkgdir/usr" \
+        -DCMAKE_SYSROOT="$sysroot" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=On \
+        -DCMAKE_CXX_COMPILER=clang++ \
+        -DCMAKE_CXX_COMPILER_TARGET="aarch64-dog-linux-musl" \
+        -DBTOP_GPU=Off \
+        -DBTOP_LTO=On
+}
+
+pkgbuild() {
+    cmake --build build
+}
+
+pkginstall() {
+    cmake --install build --strip
+
+    rm -r "$pkgdir"/usr/share/{icons,applications}
+}
