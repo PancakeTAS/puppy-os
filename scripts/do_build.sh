eb231be5f9cc216fbbb9635f779a23707e2c9599
PancakeTAS <pancake@mgnet.work>
2025-10-13 04:02:14 +0200

    add tcpdump

--- scripts/do_build.sh
+++ scripts/do_build.sh
@@ -84,6 +84,7 @@ install_package pkgs/rootfs/utils/net/iw.sh
 install_package pkgs/rootfs/utils/net/nftables.sh
 install_package pkgs/rootfs/utils/net/traceroute.sh
 install_package pkgs/rootfs/utils/net/wireguard-tools.sh
+install_package pkgs/rootfs/utils/net/tcpdump.sh
 
 # build various data packages
 install_package pkgs/rootfs/data/iana-tz.sh
