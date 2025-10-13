1531c8a419af5e5aa0a70384eba6ae5d7e4a0d53
PancakeTAS <pancake@mgnet.work>
2025-10-13 04:06:08 +0200

    add nmap

--- scripts/do_build.sh
+++ scripts/do_build.sh
@@ -86,6 +86,7 @@ install_package pkgs/rootfs/utils/net/traceroute.sh
 install_package pkgs/rootfs/utils/net/wireguard-tools.sh
 install_package pkgs/rootfs/utils/net/tcpdump.sh
 install_package pkgs/rootfs/utils/net/iperf3.sh
+install_package pkgs/rootfs/utils/net/nmap.sh
 
 # build various data packages
 install_package pkgs/rootfs/data/iana-tz.sh
