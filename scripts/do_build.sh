e5f80731ce2848560726c1f7ce1efb0da2fe1e3d
PancakeTAS <pancake@mgnet.work>
2025-10-13 03:58:41 +0200

    add traceroute

--- scripts/do_build.sh
+++ scripts/do_build.sh
@@ -82,6 +82,7 @@ install_package pkgs/rootfs/utils/net/iproute2.sh
 install_package pkgs/rootfs/utils/net/iputils.sh
 install_package pkgs/rootfs/utils/net/iw.sh
 install_package pkgs/rootfs/utils/net/nftables.sh
+install_package pkgs/rootfs/utils/net/traceroute.sh
 
 # build various data packages
 install_package pkgs/rootfs/data/iana-tz.sh
