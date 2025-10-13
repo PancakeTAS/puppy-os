79c69a97fc8f6f1ed77b05c4e23411d78ee59223
PancakeTAS <pancake@mgnet.work>
2025-10-13 10:21:09 +0200

    runit: use halt rather than poweroff
    the hardware does not support powering off and instead reboots

--- pkgs/rootfs/runit.sh
+++ pkgs/rootfs/runit.sh
@@ -11,6 +11,7 @@ pkgprepare() {
 
     sed -i 's/-static//g' src/Makefile
     sed -i "s|/service/|\/run\/runit\/service/|" src/sv.c
+    sed -i 's/RB_POWER_OFF/RB_DOES_NOT_EXIST/' src/runit.c
 
     echo "clang -O3" > src/conf-cc
     echo "clang -flto -s" > src/conf-ld
